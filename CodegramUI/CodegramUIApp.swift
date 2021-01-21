5f84d6c60b736ba2a103339625778b10fb8a5486
Giorgio Giannotta <jorge_upa@msn.com>
2021-01-21 15:07:38 +0100

    Initial Commit

---
+++ CodegramUI/CodegramUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodegramUIApp.swift
+//  CodegramUI
+//
+//  Created by Jorge Giannotta on 21/01/21.
+//
+
+import SwiftUI
+
+@main
+struct CodegramUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
